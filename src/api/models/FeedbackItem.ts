9e2ef6534494e4ab13c4dda042abd78dc3cd25ba
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2022-09-13 12:59:03 +0100

    added feedback listing

---
+++ src/api/models/FeedbackItem.ts
@@ -0,0 +1,68 @@
+import { notEmptyString } from "../validators";
+import { MediaItem } from "./MediaItem";
+
+export class FeedbackItem {
+  _id = "";
+  key = "";
+  text = "";
+  active = false;
+  deviceDetails = "";
+  createdAt = new Date();
+  modifiedAt = new Date();
+  userId = "";
+  email = "";
+  fullName = "";
+  nickName = "";
+  roles: string[] = [];
+  mediaItems: MediaItem[] = [];
+
+  constructor(inData: any = null) {
+    if (inData instanceof Object) {
+      Object.entries(inData).forEach(([key, val]) => {
+        if (typeof val === "string") {
+          switch (key) {
+            case "_id":
+            case "key":
+            case "deviceDetails":
+            case "email":
+            case "fullName":
+            case "nickName":
+            case "userId":
+            case "text":
+              this[key] = val;
+              break;
+            case "modifiedAt":
+            case "createdAt":
+              this[key] = new Date(val);
+              break;
+          }
+        }
+        if (typeof val === "boolean") {
+          switch (key) {
+            case "active":
+            case "userActive":
+              this[key] = val;
+              break;
+          }
+        }
+
+        if (val instanceof Array) {
+          switch (key) {
+            case "roles":
+              this.roles = val;
+              break;
+            case "mediaItems":
+              this.mediaItems = val
+                .filter((item) => item instanceof Object)
+                .map((item) => new MediaItem(item));
+              break;
+          }
+        }
+      });
+    }
+  }
+
+  get hasDeviceDetails() {
+    return notEmptyString(this.deviceDetails, 2);
+  }
+}
